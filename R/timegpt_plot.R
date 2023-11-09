8895908d3ec32c29d60866ca8692d6668097b864
MMenchero <marianamenchero@gmail.com>
2023-11-09 00:30:35 -0600

    WIP: Added support in the plot function for cross validation output.

--- R/timegpt_plot.R
+++ R/timegpt_plot.R
@@ -6,33 +6,59 @@
 #' @param id_col Column that identifies each series.
 #' @param time_col Column that identifies each timestep.
 #' @param target_col Column that contains the target variable.
+#' @param unique_ids Time series to plot. If NULL (default), selection will be random.
 #' @param max_insample_length Max number of insample observations to be plotted.
 #' @param plot_anomalies Whether or not to plot anomalies.
 #'
 #' @return Plot with the forecast and insample values
 #' @export
 #'
-timegpt_plot <- function(df, fcst=NULL, h=NULL, id_col=NULL, time_col="ds", target_col="y", max_insample_length=NULL, plot_anomalies=FALSE){
+timegpt_plot <- function(df, fcst=NULL, h=NULL, id_col=NULL, time_col="ds", target_col="y", unique_ids = NULL, max_insample_length=NULL, plot_anomalies=FALSE){
 
-  # Rename columns
+  # Select facets ----
+  nrow <- 4
+  ncol <- 2
+
+  # Rename columns ----
   names(df)[which(names(df) == time_col)] <- "ds"
   names(df)[which(names(df) == target_col)] <- "y"
   if(!is.null(id_col)){
     names(df)[which(names(df) == id_col)] <- "unique_id"
 
-    # Select time series if there are more than 8
+    ## Select time series if there are more than 8 ----
     if(length(unique(df$unique_id)) > 8){
-      ids <- sample(unique(df$unique_id), size=8, replace=FALSE)
+      if(!is.null(unique_ids)){
+        ids <- unique_ids[1:min(length(unique_ids), 8)]
+        if(length(ids) == 2){ # reshape for better viz
+          nrow = 2
+          ncol = 1
+        }
+      }else{
+        ids <- sample(unique(df$unique_id), size=8, replace=FALSE)
+      }
+
       df <- df |>
         dplyr::filter(.data$unique_id %in% ids)
 
-      fcst <- fcst |>
-        dplyr::filter(.data$unique_id %in% ids)
+      if(!is.null(fcst)){
+        fcst <- fcst |>
+          dplyr::filter(.data$unique_id %in% ids)
+      }
+    }
+  }
+
+  # Check for cross validation output
+  cross_validation <- FALSE
+  if("cutoff" %in% names(fcst)){
+    cross_validation <- TRUE
+    if(plot_anomalies){
+      message("Can't plot anomalies and cross validation output at the same time. Setting plot_anomalies=FALSE")
+      plot_anomalies <- FALSE
     }
   }
 
   if(is.null(fcst)){
-    # Plot historical values
+    # Plot historical values only ----
     if(!is.null(max_insample_length)){
       df <- df |>
         dplyr::group_by(.data$unique_id) |>
@@ -42,13 +68,20 @@ timegpt_plot <- function(df, fcst=NULL, h=NULL, id_col=NULL, time_col="ds", targ
 
     plot <- ggplot2::ggplot(ggplot2::aes(x=.data$ds, y=.data$y), data = df)+
       ggplot2::geom_line(color="steelblue")+
-      ggplot2::facet_wrap(~ .data$unique_id, scales = "free", ncol=2, nrow=4) +
+      ggplot2::facet_wrap(~ .data$unique_id, scales = "free", ncol=ncol, nrow=nrow) +
       ggplot2::labs(title = "Plots for unique_id", x = "Date", y = "Target") +
       ggplot2::theme_minimal()
 
   }else{
-    # Plot historical values and forecast
+    # Plot historical values and forecast ----
     color_vals <- c("#B5838D", "steelblue")
+
+    # Rename forecast columns ----
+    names(fcst)[which(names(fcst) == time_col)] <- "ds"
+    names(fcst)[which(names(fcst) == target_col)] <- "y"
+    if(!is.null(id_col)){
+      names(fcst)[which(names(fcst) == id_col)] <- "unique_id"
+    }
 
     if(!is.null(max_insample_length)){
       df <- df |>
@@ -56,15 +89,17 @@ timegpt_plot <- function(df, fcst=NULL, h=NULL, id_col=NULL, time_col="ds", targ
         dplyr::slice_tail(n=max_insample_length) |>
         dplyr::ungroup()
 
-      start_dates <- df |>
-        dplyr::group_by(.data$unique_id) |>
-        dplyr::summarize(start_date = min(.data$ds)) |>
-        dplyr::ungroup()
+      if(!cross_validation){
+        start_dates <- df |>
+          dplyr::group_by(.data$unique_id) |>
+          dplyr::summarize(start_date = min(.data$ds)) |>
+          dplyr::ungroup()
 
-      fcst <- fcst |>
-        dplyr::inner_join(start_dates, by = "unique_id") |>
-        dplyr::filter(.data$ds >= .data$start_date) |>
-        dplyr::select(-.data$start_date)
+        fcst <- fcst |>
+          dplyr::inner_join(start_dates, by = "unique_id") |>
+          dplyr::filter(.data$ds >= .data$start_date) |>
+          dplyr::select(-.data$start_date)
+      }
     }
 
     df_long <- tidyr::pivot_longer(df[,c("unique_id", "ds", "y")], cols="y", names_to="variable", values_to="value")
@@ -72,11 +107,11 @@ timegpt_plot <- function(df, fcst=NULL, h=NULL, id_col=NULL, time_col="ds", targ
     data_long <- rbind(df_long, fcst_long)
 
     plot <- ggplot2::ggplot(data_long)+
-      ggplot2::facet_wrap(~ .data$unique_id, scales = "free", ncol=2, nrow=4)+
+      ggplot2::facet_wrap(~ .data$unique_id, scales = "free", ncol=ncol, nrow=nrow)+
       ggplot2::labs(title = "Plots for unique_id", x = "Date", y = "Target")+
       ggplot2::theme_minimal()
 
-    # Check if there are prediction intervals
+    # Add prediction intervals ----
     levels <- grepl("(lo|hi)", names(fcst))
     if(any(levels)){
 
@@ -96,7 +131,7 @@ timegpt_plot <- function(df, fcst=NULL, h=NULL, id_col=NULL, time_col="ds", targ
         ggplot2::geom_ribbon(ggplot2::aes(x=.data$ds, ymin=.data$lower, ymax=.data$upper, group=.data$level, fill=.data$level), alpha=0.5, data=intervals)
     }
 
-    # Check if there are anomalies
+    # Add anomalies ----
     if(plot_anomalies){
       if(!"anomaly" %in% names(fcst)){
         message("No anomalies found. Please include them when selecting plot_anomalies=TRUE")
@@ -111,8 +146,8 @@ timegpt_plot <- function(df, fcst=NULL, h=NULL, id_col=NULL, time_col="ds", targ
       ggplot2::geom_line(ggplot2::aes(x=.data$ds, y=.data$value, group=.data$variable, color=.data$variable), data=data_long)+
       ggplot2::scale_color_manual(values = color_vals)
 
-    # Add vertical dotted line to signal start of forecast
-    if(!is.null(h)){
+    # Add start of forecast ----
+    if(!is.null(h) & cross_validation == FALSE){
       if(!plot_anomalies || !("anomaly" %in% names(fcst))){
         start_fcst <- data_long |>
           dplyr::group_by(.data$unique_id) |>
@@ -120,10 +155,21 @@ timegpt_plot <- function(df, fcst=NULL, h=NULL, id_col=NULL, time_col="ds", targ
           dplyr::ungroup()
 
         plot <- plot+
-          ggplot2::geom_vline(ggplot2::aes(xintercept=.data$start), data=start_fcst, linetype="dotted", color="tomato")
+          ggplot2::geom_vline(ggplot2::aes(xintercept=.data$start), data=start_fcst, linetype="dotted", linewidth=0.5, color="tomato")
       }
     }
 
+    # Add cutoff dates if available ----
+    if(cross_validation){
+      cutoff <- fcst[,c("unique_id", "ds", "cutoff")] |>
+        dplyr::group_by(.data$unique_id, .data$cutoff) |>
+        dplyr::slice(1) |>
+        dplyr::ungroup()
+
+      plot <- plot+
+        ggplot2::geom_vline(ggplot2::aes(xintercept=.data$ds), data=cutoff, linetype="dotted", linewidth=0.6, color="#822681FF")
+    }
   }
+
   return(plot)
 }
