c5f8a56a8266b7b60b04b3bf77d2bbd7270ba060
MMenchero <marianamenchero@gmail.com>
2024-10-09 19:07:19 -0600

    feat: add support for business day frequency (user-specified)

--- R/generate_output_dates.R
+++ R/generate_output_dates.R
@@ -21,7 +21,7 @@
 
     if (freq %in% c("QE", "Q")) {
       # End of quarter dates are: "YYYY-03-31", "YYYY-06-30", "YYYY-09-30", "YYYY-12-31".
-      dt <- seq(from = start_date, by = "quarter", length.out = h + 1)
+      dt <- seq(from = start_date, by = "quarter", length.out = h+1)
       month <- lubridate::month(start_date)
 
       # Calendar adjustments
@@ -41,13 +41,19 @@
           }
         }
       }
+      dt <- format(dt, "%Y-%m-%d")
     } else if (freq %in% c("ME", "M")) {
-      dt <- seq(from = start_date + lubridate::days(1), by = r_freq, length.out = h + 1) - lubridate::days(1)
+      dt <- seq(from = start_date + lubridate::days(1), by = r_freq, length.out = h+1) - lubridate::days(1)
+      dt <- format(dt, "%Y-%m-%d")
+    } else if(freq %in% c("B")){
+      dt <- seq(from = start_date, by = "day", length.out = h+1+ceiling(h/5)*2+2) # ceiling(h/5)*2+2 ~ number of weeks*2 days (Saturday and Sunday) plus an extra weekend to be on the safe side
+      dt <- dt[!weekdays(dt) %in% c("Saturday", "Sunday")]
+      dt <- format(dt, "%Y-%m-%d")
     } else {
-      dt <- seq(from = start_date, by = r_freq, length.out = h + 1)
+      dt <- seq(from = start_date, by = r_freq, length.out = h+1)
     }
 
-    dt[2:length(dt)]
+    dt[2:(h+1)]
   })
 
   dates_df <- data.frame(lapply(new_dates, as.POSIXct))
