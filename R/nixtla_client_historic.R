c2ee9ccfd20b33b07f006abf86c632729c631a4d
MMenchero <marianamenchero@gmail.com>
2024-12-18 22:22:42 -0600

    add vignette on fine-tuning

--- R/nixtla_client_historic.R
+++ R/nixtla_client_historic.R
@@ -8,9 +8,9 @@
 #' @param target_col Column that contains the target variable.
 #' @param level The confidence levels (0-100) for the prediction intervals.
 #' @param quantiles Quantiles to forecast. Should be between 0 and 1.
-#' @param finetune_steps Number of steps used to finetune 'TimeGPT' in the new data.
-#' @param finetune_depth The depth of the fine-tuning. Accepts an integer value from 1 to 5, where 1 (default) means little fine-tuning and 5 means that the entire model is fine-tuned to your dataset.
-#' @param finetune_loss Loss function to use for finetuning. Options are: "default", "mae", "mse", "rmse", "mape", and "smape".
+#' @param finetune_steps Number of steps used to fine-tune 'TimeGPT' in the new data.
+#' @param finetune_depth The depth of the fine-tuning. Uses a scale from 1 to 5, where 1 means little fine-tuning and 5 means that the entire model is fine-tuned.
+#' @param finetune_loss Loss function to use for fine-tuning. Options are: "default", "mae", "mse", "rmse", "mape", and "smape".
 #' @param clean_ex_first Clean exogenous signal before making the forecasts using 'TimeGPT'.
 #' @param model Model to use, either "timegpt-1" or "timegpt-1-long-horizon". Use "timegpt-1-long-horizon" if you want to forecast more than one seasonal period given the frequency of the data.
 #'
